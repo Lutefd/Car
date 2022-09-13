db10d7db69cdb1119233f0957a0e5a45bc2f1f35
Luis Dourado <lfaddourado@gmail.com>
2022-09-13 20:30:37 -0300

    Added animation to the main file

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const canvas = document.getElementById(`myCanvas`);
-canvas.height = window.innerHeight;
-canvas.widht = 500;
+canvas.widht = 200;
 
 const ctx = canvas.getContext('2d');
 const car = new Car(100, 100, 30, 50);
-car.draw(ctx);
+animate();
+let lastTime = 0;
+function animate() {
+  car.update();
+  canvas.height = window.innerHeight;
+  car.draw(ctx);
+  requestAnimationFrame(animate);
+}
