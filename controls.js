09cf15b9aa33927d4c6e893e8edd493588b04773
Luis Dourado <lfaddourado@gmail.com>
2022-09-13 19:32:18 -0300

    First commit

---
+++ controls.js
@@ -0,0 +1,46 @@
+class Controls {
+  constructor() {
+    this.forward = false;
+    this.left = false;
+    this.right = false;
+    this.down = false;
+
+    this.#addKeyboardListeners();
+  }
+  #addKeyboardListeners() {
+    document.onkeydown = (event) => {
+      switch (event.key) {
+        case 'ArrowLeft':
+          this.left = true;
+          break;
+        case 'ArrowRight':
+          this.right = true;
+          break;
+        case `ArrowUp`:
+          this.forward = true;
+          break;
+        case `ArrowDown`:
+          this.down = true;
+          break;
+      }
+      console.table(this);
+    };
+    document.onkeyup = (event) => {
+      switch (event.key) {
+        case `ArrowLeft`:
+          this.left = false;
+          break;
+        case `ArrowRight`:
+          this.right = false;
+          break;
+        case `ArrowUp`:
+          this.foward = false;
+          break;
+        case `ArrowDown`:
+          this.down = false;
+          break;
+      }
+      console.table(this);
+    };
+  }
+}
