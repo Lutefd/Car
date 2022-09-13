7bf92faa6f6f7feafc85c6f78b68fa439cad1ec3
Luis Dourado <lfaddourado@gmail.com>
2022-09-13 20:59:37 -0300

    Added road and road definition

--- main.js
+++ main.js
@@ -2,12 +2,15 @@ const canvas = document.getElementById(`myCanvas`);
 canvas.widht = 200;
 
 const ctx = canvas.getContext('2d');
-const car = new Car(100, 100, 30, 50);
+const road = new Road(canvas.width / 2, canvas.width * 0.9);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
+
 animate();
 let lastTime = 0;
 function animate() {
   car.update();
   canvas.height = window.innerHeight;
+  road.draw(ctx);
   car.draw(ctx);
   requestAnimationFrame(animate);
 }
